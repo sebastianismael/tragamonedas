1e472813ef5089bf293f860231a2e927ec044526
Sebastian Ismael <sebastianismael@gmail.com>
2016-04-20 09:03:18 -0300

    Agrega un test para el caso de no dar premio

--- src/test/java/edu/grupoesfera/mocking/TragamonedasTest.java
+++ src/test/java/edu/grupoesfera/mocking/TragamonedasTest.java
@@ -28,4 +28,25 @@ public class TragamonedasTest {
 		maquinita.activar();
 		Assert.assertTrue(maquinita.entregaPremio());
 	}
+	
+	@Test
+	public void siLosTresTamboresTienenDistintoValorNoDeberiaDarPremio(){
+		
+		TamborFactory factory = mock(TamborFactory.class);
+		Tambor tambor1Mock = mock(Tambor.class);
+		Tambor tambor2Mock = mock(Tambor.class);
+		Tambor tambor3Mock = mock(Tambor.class);
+		
+		when(factory.crearTambor(1)).thenReturn(tambor1Mock);
+		when(factory.crearTambor(2)).thenReturn(tambor2Mock);
+		when(factory.crearTambor(3)).thenReturn(tambor3Mock);
+		
+		when(tambor1Mock.obtenerPosicion()).thenReturn(3);
+		when(tambor2Mock.obtenerPosicion()).thenReturn(4);
+		when(tambor3Mock.obtenerPosicion()).thenReturn(1);
+		
+		Tragamonedas maquinita = new Tragamonedas(factory);
+		maquinita.activar();
+		Assert.assertFalse(maquinita.entregaPremio());
+	}
 }
