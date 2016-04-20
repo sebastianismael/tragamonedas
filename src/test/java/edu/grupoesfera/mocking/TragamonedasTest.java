72d9fc6fcd4072c2024da8ff3dc3220e666cf8f7
Sebastian Ismael <sebastianismael@gmail.com>
2016-04-20 09:17:30 -0300

    Agrega otro ejemplo de spies: verificacion por la no invocación de un metodo

--- src/test/java/edu/grupoesfera/mocking/TragamonedasTest.java
+++ src/test/java/edu/grupoesfera/mocking/TragamonedasTest.java
@@ -3,10 +3,12 @@ package edu.grupoesfera.mocking;
 
 import org.junit.Assert;
 import org.junit.Test;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.never;
 
 public class TragamonedasTest {
 
@@ -74,5 +76,29 @@ public class TragamonedasTest {
 		verify(tambor1Mock, times(1)).girar();
 		verify(tambor2Mock, times(1)).girar();
 		verify(tambor3Mock, times(1)).girar();
+	}
+	
+	@Test
+	@SuppressWarnings("unused")
+	public void verificarQueSiNoSeActivaElTragamonedasNoGirenLosTambores(){
+		
+		TamborFactory factory = mock(TamborFactory.class);
+		Tambor tambor1Mock = mock(Tambor.class);
+		Tambor tambor2Mock = mock(Tambor.class);
+		Tambor tambor3Mock = mock(Tambor.class);
+		
+		when(factory.crearTambor(1)).thenReturn(tambor1Mock);
+		when(factory.crearTambor(2)).thenReturn(tambor2Mock);
+		when(factory.crearTambor(3)).thenReturn(tambor3Mock);
+		
+		when(tambor1Mock.obtenerPosicion()).thenReturn(3);
+		when(tambor2Mock.obtenerPosicion()).thenReturn(3);
+		when(tambor3Mock.obtenerPosicion()).thenReturn(3);
+		
+		Tragamonedas maquinita = new Tragamonedas(factory);
+		
+		verify(tambor1Mock, never()).girar();
+		verify(tambor2Mock, never()).girar();
+		verify(tambor3Mock, never()).girar();
 	}
 }
