931c4e43a8d5ff326be50b0d82b911fdc61f479f
Sebastian Ismael <sebastianismael@gmail.com>
2016-04-20 09:02:35 -0300

    Refactoriza el test usando mocks

--- src/test/java/edu/grupoesfera/mocking/TragamonedasTest.java
+++ src/test/java/edu/grupoesfera/mocking/TragamonedasTest.java
@@ -4,13 +4,27 @@ package edu.grupoesfera.mocking;
 import org.junit.Assert;
 import org.junit.Test;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class TragamonedasTest {
 
 	@Test
 	public void siLosTresTamboresTienenElMismoValorDeberiaDarPremio(){
 		
-		Tragamonedas maquinita = new Tragamonedas(new TamborFactory());
+		TamborFactory factory = mock(TamborFactory.class);
+		Tambor tambor1Mock = mock(Tambor.class);
+		Tambor tambor2Mock = mock(Tambor.class);
+		Tambor tambor3Mock = mock(Tambor.class);
+		
+		when(factory.crearTambor(1)).thenReturn(tambor1Mock);
+		when(factory.crearTambor(2)).thenReturn(tambor2Mock);
+		when(factory.crearTambor(3)).thenReturn(tambor3Mock);
+		
+		when(tambor1Mock.obtenerPosicion()).thenReturn(3);
+		when(tambor2Mock.obtenerPosicion()).thenReturn(3);
+		when(tambor3Mock.obtenerPosicion()).thenReturn(3);
+		
+		Tragamonedas maquinita = new Tragamonedas(factory);
 		maquinita.activar();
 		Assert.assertTrue(maquinita.entregaPremio());
 	}
