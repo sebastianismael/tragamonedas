4694f0607eb740e134454a8ddc12fa79c85f6dbf
Sebastian Ismael <sebastianismael@gmail.com>
2016-04-20 09:13:30 -0300

    Agrega spies: verificamos que se haya ejecutado una vez el metodo girar del tambor

--- src/test/java/edu/grupoesfera/mocking/TragamonedasTest.java
+++ src/test/java/edu/grupoesfera/mocking/TragamonedasTest.java
@@ -5,6 +5,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
 
 public class TragamonedasTest {
 
@@ -48,5 +50,29 @@ public class TragamonedasTest {
 		Tragamonedas maquinita = new Tragamonedas(factory);
 		maquinita.activar();
 		Assert.assertFalse(maquinita.entregaPremio());
+	}
+	
+	@Test
+	public void verificarQueAlActivarElTragamonedasGirenLosTresTambores(){
+		
+		TamborFactory factory = mock(TamborFactory.class);
+		Tambor tambor1Mock = mock(Tambor.class);
+		Tambor tambor2Mock = mock(Tambor.class);
+		Tambor tambor3Mock = mock(Tambor.class);
+		
+		when(factory.crearTambor(1)).thenReturn(tambor1Mock);
+		when(factory.crearTambor(2)).thenReturn(tambor2Mock);
+		when(factory.crearTambor(3)).thenReturn(tambor3Mock);
+		
+		when(tambor1Mock.obtenerPosicion()).thenReturn(3);
+		when(tambor2Mock.obtenerPosicion()).thenReturn(3);
+		when(tambor3Mock.obtenerPosicion()).thenReturn(3);
+		
+		Tragamonedas maquinita = new Tragamonedas(factory);
+		maquinita.activar();
+		
+		verify(tambor1Mock, times(1)).girar();
+		verify(tambor2Mock, times(1)).girar();
+		verify(tambor3Mock, times(1)).girar();
 	}
 }
